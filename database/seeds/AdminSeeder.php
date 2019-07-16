723716809a4af165136076a5f3bdc680f747757b
Vadim Krutov <vadims.krutovs@gmail.com>
2019-07-16 18:03:52 +0300

    Add admin db seeder

---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * @throws Exception
+     * @throws \Doctrine\ORM\OptimisticLockException
+     * @throws \LaravelDoctrine\ORM\Facades\ORMException
+     * @throws \LaravelDoctrine\ORM\Facades\ORMInvalidArgumentException
+     */
+    public function run()
+    {
+        /** @var \App\Entity\User $admin */
+        $admin = \EntityManager::getRepository(\App\Entity\User::class)->findOneBy([
+            'email' => 'admin@aslairlines.be'
+        ]);
+
+        if ($admin == null) {
+            $admin = new \App\Entity\User();
+        }
+
+        $admin->setEmail('admin@aslairlines.be');
+        $admin->setName('Root');
+        $admin->setLastname('Admin');
+        $admin->setPassword('aslairlines2018');
+
+        $role = \EntityManager::getRepository(\App\Entity\UserRole::class)->findOneBy([
+            'name' => \App\Entity\UserRole::ROLE_ADMIN
+        ]);
+
+        if ($role == null) {
+            $msg = "Please import roles before seed admins
+             you can do by console commnad \"php artisan app:import_roles\"";
+            throw new Exception($msg);
+        }
+
+        $admin->setRole($role);
+
+        \EntityManager::persist($admin);
+        \EntityManager::flush();
+    }
+}
